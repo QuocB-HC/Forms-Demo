119153f3eacb22b043e530da4a1bf6443a25c2e2
BaoNVQ2903 <nguyenvoquocbao292003@gmail.com>
2025-03-24 20:02:00 +0700

    add forms with validation

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -9,10 +9,19 @@ import { Component } from '@angular/core';
 export class FormsComponent {
   userName: string = "";
   favoriteMusic: string = "";
+  user = {
+    name: '',
+    email: '',
+  }
 
-  // // Nhập Favorite Music bằng prompt
+  // Nhập Favorite Music bằng prompt
   // showForms() {
   //   let a = prompt("Your Favorite Music ?","");
   //   this.favoriteMusic = String(a);
   // }
+
+  submitForm() {
+    console.log('Thông tin nhập vào:', this.user);
+    alert('Gửi thành công!');
+  }
 }
